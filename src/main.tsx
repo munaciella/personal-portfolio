aea492e1854986c45773fe6275e80250cb1d807b
munaciella <franes88@gmail.com>
2024-02-08 15:14:19 +0000

    add typescript file

---
+++ src/main.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+
+const element = document.getElementById('root') || document.createElement('p');
+
+
+
+  ReactDOM.createRoot(element).render(
+    <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+// import './index.css';
+
+
+// ReactDOM.createRoot(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
