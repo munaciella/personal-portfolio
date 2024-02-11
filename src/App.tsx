83ba225841f643339663c8dc8689df96905a6b86
munaciella <franes88@gmail.com>
2024-02-11 15:24:26 +0000

    add tailwind config file

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, { createContext, useContext, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './Components/Header/Header';
 import GitHub from './Components/GitHub/GitHub';
 import LinkedIn from './Components/LinkedIn/LinkedIn';
-import {ContactPage} from './Components/ContactPage/ContactPage';
+import { ContactPage } from './Components/ContactPage/ContactPage';
 import { ThemeProvider } from './Components/ThemeContext/ThemeContext';
 // import { useTheme } from './Components/ThemeContext/ThemeContext';
 import './App.css';
-import Navbar from "./Components/NavBar/NavBar";
-import Content from "./Components/Content/Content";
+import Navbar from './Components/NavBar/NavBar';
+import Content from './Components/Content/Content';
 import Statement from './Components/Statement/Statement';
 
 const App: React.FC = () => {
@@ -21,21 +21,21 @@ const App: React.FC = () => {
   return (
     <>
       <ThemeProvider>
-            <Header />
-            <Statement/>
-          <BrowserRouter>
-          <Content/>
-          <Navbar/>
-            <GitHub />
-            <LinkedIn />
-            <ContactPage/>
-        {/* <div className={theme === 'dark' ? 'dark-theme' : 'light-theme'}>
+        <Header />
+        <Statement />
+        <BrowserRouter>
+          <Content />
+          <Navbar />
+          <GitHub />
+          <LinkedIn />
+          <ContactPage />
+          {/* <div className={theme === 'dark' ? 'dark-theme' : 'light-theme'}>
           <button onClick={toggleThemeColor}>Toggle Theme</button> */}
-            <Routes>
-              <Route />
-              {/* path="/" element={<Header/>}/> */}
-            </Routes>
-          </BrowserRouter>
+          <Routes>
+            <Route />
+            {/* path="/" element={<Header/>}/> */}
+          </Routes>
+        </BrowserRouter>
       </ThemeProvider>
     </>
   );
