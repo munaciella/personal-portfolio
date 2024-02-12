fa1e13ad298fe872b057209304c9611f513b8ded
munaciella <franes88@gmail.com>
2024-02-12 00:05:05 +0000

    remove git.css

--- src/Components/GitHub/GitHub.tsx
+++ src/Components/GitHub/GitHub.tsx
@@ -4,11 +4,12 @@ import './GitHub.css'
 const GitHub: React.FC = () => {
 
     return (
-        <div className='github-icon-container'>
-        <a href='https://github.com/munaciella' target='_blank' rel='noopener noreferrer'>
-            <FaGithub size={30} />
-        </a>
-        </div>
+        <div className="github-icon-container fixed top-5 right-5 z-50">
+  <a href="https://github.com/munaciella" target="_blank" rel="noopener noreferrer" className="text-black transition-colors duration-300 relative">
+    <FaGithub size={35} className="hover:text-gray-500" />
+  </a>
+</div>
+
     )
 }
 
