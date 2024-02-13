716f44060449e757ec94dd0acead8508b2eadc69
munaciella <franes88@gmail.com>
2024-02-13 00:14:00 +0000

    add style to portfolio

--- src/Components/Portfolio.tsx
+++ src/Components/Portfolio.tsx
@@ -1,12 +1,20 @@
-import React from 'react'
-import portfolio from '../data/portfolio'
-import { PortfolioItem } from './PortfolioItem'
+import React from 'react';
+import portfolio from '../data/portfolio';
+import { PortfolioItem } from './PortfolioItem';
 
 export const Portfolio: React.FC = () => {
-
-    return (
-        <div className='flex flex-col md:flex-row items-center justify-center'>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4'></div>
-        </div>
-    )
-}
+  return (
+    <div className="flex flex-col md:flex-row items-center justify-center ml-36">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+        {portfolio.map((project) => (
+          <PortfolioItem
+            imgUrl={project.imgUrl}
+            title={project.title}
+            stack={project.stack}
+            link={project.link}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
--- src/Components/PortfolioItem.tsx
+++ src/Components/PortfolioItem.tsx
@@ -21,7 +21,7 @@ export const PortfolioItem: React.FC<PortfolioItemProps> = ({
         className="w-full h-36 md:h-48 object-cover cursor-pointer"
       />
       <div className="w-full p-4">
-        <h3>{title}</h3>
+        <h3 className='text-lg md:text-xl mb-2 md:mb-3 font-semibold'>{title}</h3>
         <p>
           {stack.map((item) => (
             <span>{item}</span>
