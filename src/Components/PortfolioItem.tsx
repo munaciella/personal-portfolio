6260e9664b1a0195a442b5a210cbe091188d7076
munaciella <franes88@gmail.com>
2024-02-14 01:15:00 +0000

    add styling to portfolioitem file

--- src/Components/PortfolioItem.tsx
+++ src/Components/PortfolioItem.tsx
@@ -14,22 +14,29 @@ export const PortfolioItem: React.FC<PortfolioItemProps> = ({
   link,
 }) => {
   return (
-    <div>
+    <div className='border-2 border-stone-900 rounded-md overflow-hidden'>
       <img
         src={imgUrl}
         alt="portfolio"
         className="w-full h-36 md:h-48 object-cover cursor-pointer"
       />
       <div className="w-full p-4">
-        <h3 className='text-lg md:text-xl mb-2 md:mb-3 font-semibold'>{title}</h3>
-        <p>
-          {stack.map((index, item) => (
-            <span key={index}>{item}</span>
+        <h3 className="text-lg md:text-xl mb-2 md:mb-3 font-semibold">
+          {title}
+        </h3>
+        <p className="flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm">
+          {stack.map((item) => (
+            <span
+              key={item}
+              className="inline-block px-2 py-1 font-semibold border-2 border-stone-900 rounded-xl"
+            >
+              {item}
+            </span>
           ))}
         </p>
       </div>
       <a href={link} target="_blank" rel="noopener noreferrer">
-        Link
+        <button className='flex flex-wrap flex-row items-center border-2 border-green-400 rounded-md cursor-pointer bg-green-500 hover:text-green-200 justify-center'> Project Link</button>
       </a>
     </div>
   );
