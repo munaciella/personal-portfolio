0cfbce39f18f7a633cbc1e96c57f50f8efd13fc9
munaciella <franes88@gmail.com>
2024-02-21 15:05:56 +0000

    add more timeline options

--- src/data/timeline.ts
+++ src/data/timeline.ts
@@ -1,6 +1,6 @@
 export default [
     {
-        year: 'Today',
+        year: '2024',
         title: 'Trainee Full-Stack Software Developer',
         duration: 'October 2023 - January 2024',
         details: 'I have successfully completed a software engineering bootcamp at Northcoders in Manchester (UK). I built fully functional web and mobile applications in an Agile team setting. Our tech stack consisted of JavaScript, Typescript, React, React Native, Node.js, Express and PostgreSQL.',
@@ -8,6 +8,24 @@ export default [
     {
         year: '2023',
         title: 'First Line of Code',
+        duration: 'the beginning',
+        details: 'I wrote my first line of real code that opened the door to this journey into digital craftmanship that I never would have imagined.',
+    },
+    {
+        year: '2018',
+        title: 'PlantPoweredPT Personal Training @ The Gym Group',
+        duration: 'the beginning',
+        details: 'I wrote my first line of real code that opened the door to this journey into digital craftmanship that I never would have imagined.',
+    },
+    {
+        year: '2014',
+        title: 'PlantPoweredPT Personal Training @ The Gym Group',
+        duration: 'the beginning',
+        details: 'I wrote my first line of real code that opened the door to this journey into digital craftmanship that I never would have imagined.',
+    },
+    {
+        year: '2001',
+        title: 'PlantPoweredPT Personal Training @ The Gym Group',
         duration: 'the beginning',
         details: 'I wrote my first line of real code that opened the door to this journey into digital craftmanship that I never would have imagined.',
     },
