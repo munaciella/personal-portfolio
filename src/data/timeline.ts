fcd821b4f649712f6cb8cbaee220c1bf10dc9da7
munaciella <franes88@gmail.com>
2024-02-22 01:36:05 +0000

    add contact form

--- src/data/timeline.ts
+++ src/data/timeline.ts
@@ -2,8 +2,8 @@ export default [
     {
         year: '2024',
         title: 'Trainee Full-Stack Software Developer',
-        duration: 'October 2023 - January 2024',
-        details: 'I have successfully completed a software engineering bootcamp at Northcoders in Manchester (UK). I built fully functional web and mobile applications in an Agile team setting. Our tech stack consisted of JavaScript, Typescript, React, React Native, Node.js, Express and PostgreSQL.',
+        duration: '4 months',
+        details: 'I successfully completed a software engineering bootcamp at Northcoders in Manchester (UK). I built fully functional web and mobile applications in an Agile team setting. Our tech stack consisted of JavaScript, Typescript, React, React Native, Node.js, Express and PostgreSQL.',
     },
     {
         year: '2023',
@@ -14,19 +14,19 @@ export default [
     {
         year: '2018',
         title: 'PlantPoweredPT Personal Training @ The Gym Group',
-        duration: 'the beginning',
+        duration: '5 years',
         details: 'I wrote my first line of real code that opened the door to this journey into digital craftmanship that I never would have imagined.',
     },
     {
         year: '2014',
-        title: 'PlantPoweredPT Personal Training @ The Gym Group',
-        duration: 'the beginning',
+        title: 'BSc Honours in Sport, Fitness and Coaching',
+        duration: '6 years',
         details: 'I wrote my first line of real code that opened the door to this journey into digital craftmanship that I never would have imagined.',
     },
     {
         year: '2001',
         title: 'Moved to Manchester & First Job in the UK',
-        duration: 'January 2001 - February 2023',
+        duration: '22 years',
         details: 'I wrote my first line of real code that opened the door to this journey into digital craftmanship that I never would have imagined.',
     },
 ]
