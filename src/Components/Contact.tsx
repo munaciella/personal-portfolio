dc0bb1f880d6ab397734e0b655e5da6bf5360e77
munaciella <franes88@gmail.com>
2024-02-22 23:44:03 +0000

    add button and style to button for contact

--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -29,7 +29,7 @@ export const Contact: React.FC = () => {
             />
             <button 
             type='button' 
-            className='text-center inline-block px-8 py-3 w-max text-base font-medium rounded-md text-white bg-gradient-to-r from-yellow-500 to-pink-500 drop-shadow-md hover:from-yellow-600 hover:to-pink-600'>
+            className='text-center inline-block px-8 py-3 w-max text-base font-medium rounded-md text-white bg-gradient-to-r from-yellow-300 to-orange-600 drop-shadow-md hover:from-yellow-600 hover:to-orange-600'>
                 Work With Me
             </button>
         </form>
