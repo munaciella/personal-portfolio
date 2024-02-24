e06c7bea6102c2235e0bc6fde2238f21731357a4
munaciella <franes88@gmail.com>
2024-02-24 00:53:44 +0000

    add resume button and styling

--- src/Components/Intro.tsx
+++ src/Components/Intro.tsx
@@ -6,10 +6,10 @@ export const Intro: React.FC = () => {
       <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold sm:text-2xl sm:mb-2 mt-2">
         Francesco Vurchio
       </h1>
-      <p className="text-lg md:text-xl mb-3 font-medium">Junior Software Engineer & Web Developer</p>
+      <p className="text-lg md:text-xl mb-3 mt-2 font-medium">Junior Software Engineer & Web Developer</p>
       <div className='relative'>
       <div className='flex justify-center pb-2'>
-      <a href='/assets/Francesco Vurchio-2024-CV.pdf' target="_blank" rel="noopener noreferrer" className='text-center flex items-center border-1 border-green-400 rounded-md cursor-pointer bg-green-500 hover:text-green-200 p-1 px-5 mt-1'>
+      <a href='/assets/Francesco Vurchio-2024-CV.pdf' target="_blank" rel="noopener noreferrer" className='text-center flex items-center border-1 cursor-pointer p-1 px-5 mt-2 rounded-md py-2 w-max text-base font-medium text-white bg-gradient-to-r from-green-300 to-green-500 drop-shadow-md hover:from-green-400 hover:to-green-600'>
         Resume
       </a>
     </div>
--- src/Components/Portfolio.tsx
+++ src/Components/Portfolio.tsx
@@ -5,7 +5,7 @@ import { PortfolioItem } from './PortfolioItem';
 
 export const Portfolio: React.FC = () => {
   return (
-    <div className="flex flex-col md:flex-row items-center justify-center ml-10 mr-10 mb-40">
+    <div className="flex flex-col md:flex-row items-center justify-center ml-10 mr-10 mb-40 mt-4">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
         {portfolio.map((project) => (
           <PortfolioItem
