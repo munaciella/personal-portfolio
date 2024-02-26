4a46d38a43bb9643b92e28d89f6f9fdc1f79d00c
munaciella <franes88@gmail.com>
2024-02-26 15:31:07 +0000

    add form to contact

--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -5,7 +5,7 @@ export const Contact: React.FC = () => {
     <div className="flex flex-col mb-10 mx-auto">
       <div className="flex justify-center items-center">
         <form 
-        action="https://getform.io/slug"
+        action="https://getform.io/f/lejxRBbj"
         method="POST"
         className='flex flex-col w-full md:w-7/12'
         >
